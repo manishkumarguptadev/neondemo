656e7969b003acaf20c621a308cd97bf1b5c53c2
manishkumarguptadev <manishkumarguptadev@gmail.com>
2024-07-02 23:22:41 +0530

    initial commit

---
+++ src/test/java/com/example/neondemo/NeondemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeondemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
